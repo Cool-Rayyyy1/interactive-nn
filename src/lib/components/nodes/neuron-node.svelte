47d1e6dbaebdc4c3f2fa1741c82af491f92e33dd
Wollaston <matt@wollaston.dev>
2025-02-06 22:23:17 -0500

    Add svelte flow and create example

---
+++ src/lib/components/nodes/neuron-node.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+
+	type $$Props = NodeProps;
+</script>
+
+<div class="rounded-md border-2 border-stone-400 bg-white px-4 py-2 shadow-md">
+	<div class="flex">
+		<div class="flex h-12 w-12 items-center justify-center rounded-full bg-gray-100">C</div>
+		<div class="ml-2">
+			<div class="text-lg font-bold">John</div>
+			<div class="text-gray-500">Designer</div>
+		</div>
+	</div>
+	<Handle type="target" position={Position.Bottom} />
+</div>
--- src/routes/perceptrons/single-layer-perceptron/+page.svelte
+++ src/routes/perceptrons/single-layer-perceptron/+page.svelte
@@ -1,1 +1,75 @@
-<h1>Single-Layer Perceptron</h1>
+<script>
+	import { writable } from 'svelte/store';
+	import { SvelteFlow, Controls, Background, BackgroundVariant } from '@xyflow/svelte';
+	import '@xyflow/svelte/dist/style.css';
+	import InputNode from '$lib/components/nodes/input-node.svelte';
+	import NeuronNode from '$lib/components/nodes/neuron-node.svelte';
+
+	const nodeTypes = {
+		input: InputNode,
+		neuron: NeuronNode
+	};
+
+	// We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+	const nodes = writable([
+		{
+			id: 'neuron-1',
+			type: 'neuron',
+			data: { label: 'Node' },
+			position: { x: 0, y: 0 }
+		},
+		{
+			id: 'input-1',
+			type: 'input',
+			data: { label: 'Input Node' },
+			position: { x: -100, y: 150 }
+		},
+		{
+			id: 'input-2',
+			type: 'input',
+			data: { label: 'Input Node' },
+			position: { x: 100, y: 150 }
+		}
+	]);
+
+	// same for edges
+	const edges = writable([
+		{
+			id: 'input-1_neuron-1',
+			type: 'default',
+			source: 'input-1',
+			target: 'neuron-1',
+			label: 'Edge Text'
+		},
+		{
+			id: 'input-2_neuron-1',
+			type: 'default',
+			source: 'input-2',
+			target: 'neuron-1',
+			label: 'Edge Text'
+		}
+	]);
+
+	const snapGrid = [25, 25];
+</script>
+
+<!--
+👇 By default, the Svelte Flow container has a height of 100%.
+This means that the parent container needs a height to render the flow.
+-->
+<div>
+	<h1>Single-Layer Perceptron</h1>
+	<div style:height="500px" class="border-2">
+		<SvelteFlow
+			{nodes}
+			{edges}
+			{snapGrid}
+			{nodeTypes}
+			fitView
+			on:nodeclick={(event) => console.log('on node click', event.detail.node)}
+		>
+			<Controls />
+			<Background variant={BackgroundVariant.Dots} />
+		</SvelteFlow>
+	</div>
+</div>
