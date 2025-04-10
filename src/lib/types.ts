0137932fb8d96698bb98b5ee76cee0941aa78a1e
Wollaston <matt@wollaston.dev>
2025-04-10 12:09:00 -0400

    Add documentation to interfaces and utility functions

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -18,59 +18,100 @@ export type NeuronNodeType = Node<{ layer: InputLayer | HiddenLayer, handles: Ha
 
 export type CustomNodes = InputNodeType | WeightNodeType | NeuronNodeType;
 
-export interface Data {
+/**
+ * 2d (x, y) coordinates in a cartesian plane
+ */
+export interface Coords2d {
   x: number,
   y: number,
 }
 
+/**
+ * Props for a <Handle /> component in xyflow
+ */
 export interface HandleProp {
   type: "source" | "target",
   position: Position,
   id?: string,
 }
 
+/**
+ * Configuration values for a custom 3d element in Threlte
+ */
 export interface Config {
   coords: { x: number; y: number; z: number };
   shape: [number, number, number];
   color: string;
 }
 
+/**
+ * Parent type for a Neural Network visual representation
+ */
 export interface Network {
   input: InputLayer,
   hidden?: HiddenLayer[],
 }
 
+/**
+ * A Neuron in a Neural Network with an associated Activation Function
+ */
+export interface Neuron {
+  activation: ActivationFunction
+}
+
+/**
+ * An Input Layer to a Neural Network
+ */
 export interface InputLayer {
   bias: BiasNode;
   values: ProductNode[];
   neuron: Neuron;
 }
 
+/**
+ * A Hidden Layer in a Neural Network
+ */
 export interface HiddenLayer {
   bias: BiasNode;
   weights: number[]
   neuron: Neuron;
 }
 
+/**
+ * An Input Node to a Neural Network. This is distinguished from a normal
+ * input to a Neural Network as it has one associated weight, but a range 
+ * of associated inputs. This is because for the visual representation goal
+ * of this project, we generate outputs for a provided range. The users only 
+ * interact with weights.
+ */
 export interface ProductNode {
   weight: number;
   inputs: Input[];
 }
 
+/**
+ * A Bias Node in a Neural Network with a weight value, and an Input.
+ */
 export interface BiasNode {
   weight: number;
   input: Input;
 }
 
-export interface Neuron {
-  activation: ActivationFunction
-}
-
+/**
+ * An input to a Neural Network. This is effectively a tuple to keep track of
+ * the original input value can be tracked as the ouput is transformed as 
+ * it is passed through the Neural Network.
+ */
 export interface Input {
   readonly input: number;
   output: number;
 }
 
+/**
+ * Tracks the various Activation Functions available in a network. Helpful for 
+ * matching which Activation Function should be applied by a given Neuron
+ * in the Network.
+ */
 export enum ActivationFunction {
   Step,
   Sigmoid,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,23 +1,25 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { ActivationFunction, type BiasNode, type Input, type InputLayer, type InputNodeType, type Network, type Neuron, type ProductNode, type WeightNodeType } from "./types";
+import { ActivationFunction, type BiasNode, type Input, type InputLayer, type Network, type Neuron, type ProductNode } from "./types";
 
+/**
+ * Tailwind utility function. Provided by shadcn-svelte
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function simpleId(): string {
-  return Math.round(Math.random() * 100 * Math.random() * 100 * Math.random() * 100).toString()
-}
-
-export function isInputNode(
-  node: any,
-): node is InputNodeType | WeightNodeType {
-  return !node || !node.type
-    ? false
-    : node.type === 'nnInput' || node.type === 'weight';
-}
-
+/**
+ * Returns a range of numbers as a number[].
+ *
+ * @remarks
+ * This is similar to Python's 'range()' but less flexible.
+ *
+ * @param start - The start of the range, inclusive
+ * @param end - The end of the range, non-inclusive
+ * @param step - The step between each value in the range
+ * @returns The calculated range as a number[]
+ */
 export function range(start: number, end: number, step: number): number[] {
   const result: number[] = [];
   for (let i = start; i < end; i += step) {
@@ -26,23 +28,64 @@ export function range(start: number, end: number, step: number): number[] {
   return result;
 }
 
+/**
+ * Calculates the sigmoid value of a given input
+ *
+ * @remarks 
+ * Sigmoid is calculated by:
+ * (1 / (1 + Math.exp(-x)))
+ *
+ * @param x - The input value
+ * @returns The sigmoid value of the input
+ */
 export function sigmoid(x: number): number {
   return (1 / (1 + Math.exp(-x)))
 }
 
+/**
+ * Calculates the relu value of a given input
+ *
+ * @remarks
+ * This is a standard rectified linear unit ("ReLU")
+ * Math.max(0, x)
+ *
+ * @param x - The input value
+ * @returns The sigmoid value of the input
+ */
 export function relu(x: number): number {
   return Math.max(0, x)
 }
 
+/**
+ * Calculates the weighted value of a BiasNode in the network
+ *
+ * @param node - The BiasNode input
+ * @returns the weight value of the BiasNode
+ */
 export function weightedBias(node: BiasNode): Input {
   return { input: node.input.input, output: node.weight * node.input.output }
 }
 
-
+/**
+ * Calculates the weighted input range to the network for a given
+ * ProductNode. It returns a range of Inputs, which contain the original input
+ * and its transformed value at the given location in the network, due to the
+ * interactive structure of this application
+ *
+ * @param node - The ProductNode Input
+ * @returns The ranged of weighted inputs
+ */
 export function weightedInput(node: ProductNode): Input[] {
   return node.inputs.map((val) => <Input>{ input: val.input, output: val.output * node.weight })
 }
 
+/**
+ * Runs an input value through the given Neuron's activation function
+ * 
+ * @param neuron - The neuron with the required activation function
+ * @param input - The input value to transform with the activation
+ * @returns The activated valued of the input
+ */
 export function activate(neuron: Neuron, input: number): number {
   switch (neuron.activation) {
     case ActivationFunction.Step:
@@ -56,16 +99,38 @@ export function activate(neuron: Neuron, input: number): number {
   }
 }
 
+/**
+ * Computes the activated, weighted output range for the given network layer.
+ *
+ * @param layer - The network layer to compute the value of
+ * @returns The activated, weighted output range for the layer
+ */
 export function layerValue(layer: InputLayer): Input[] {
   const bias = weightedBias(layer.bias);
   const inputDotProduct = mergeProductArrays(layer.values.map((val) => weightedInput(val)));
   return inputDotProduct.map((val): Input => <Input>{ input: val.input, output: activate(layer.neuron, bias.output + val.output) })
 }
 
+/**
+ * Computes the activated, weighted output range for the given network.
+ *
+ * @param network - The network to compute the value of
+ * @returns The activated, weighted output range for the network
+ */
 export function networkValue(network: Network): Input[] {
   return layerValue(network.input)
 }
 
+/**
+ * Helper function to combine the values of adjacted Input[]
+ *
+ * @remarks
+ * This is similar to a zip function. Arrays of Input[][]
+ * must be same length.
+ *
+ * @param arrays - The Input[][] arrays to combine element-wise
+ * @returns The element-wise combined Input[]
+ */
 function mergeProductArrays(arrays: Input[][]): Input[] {
   const cols = range(0, arrays.length, 1);
   const merged: Input[] = []
@@ -76,10 +141,23 @@ function mergeProductArrays(arrays: Input[][]): Input[] {
   return merged;
 }
 
+/**
+ * Helper to generate a range of Input
+ *
+ * @param range - number[] range to generate Input[] from
+ * @returns The generated Input[] range
+ */
 export function genInputs(range: number[]): Input[] {
   return range.map((val) => <Input>{ input: val, output: val })
 }
 
+/**
+ * Helper to generate activated values for an Input[]
+ * 
+ * @param inputs - The range of inputs to calculate activated values of 
+ * @param neuron - The Neuron with the required activation function
+ * @returns Input[] with activated output values
+ */
 export function activateInputs(inputs: Input[], neuron: Neuron): Input[] {
   return inputs.map((val) => <Input>{ input: val.input, output: activate(neuron, val.output) })
 }
