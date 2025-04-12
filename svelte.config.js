9be2703fa29c575a5caa30f6c8eb08e5b854582c
Wollaston <matt@wollaston.dev>
2025-04-11 22:19:36 -0400

    Add mdsvex layout

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,13 @@ import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+  preprocess: [vitePreprocess(), mdsvex({
+    layout: {
+      _: 'src/lib/markdown-layout.svelte'
+    },
+  })],
+  kit: { adapter: adapter() },
+  extensions: ['.svelte', '.svx']
 };
 
 export default config;
