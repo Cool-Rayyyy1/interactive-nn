6f8585f7feafde876867aa55154d7bd305a469c3
Wollaston <matt@wollaston.dev>
2025-04-12 22:56:42 -0400

    Update 3d scene defaults

--- src/lib/components/scenes/default-scene.svelte
+++ src/lib/components/scenes/default-scene.svelte
@@ -6,6 +6,7 @@
 	import type { Config, Input } from '$lib/types';
 	import { PerspectiveCamera, type ColorRepresentation, type Mesh } from 'three';
 	import CameraControls from '$lib/shared/CameraControls';
+	import { HTML } from '@threlte/extras';
 
 	let {
 		color = '#ff3e00',
@@ -27,9 +28,9 @@
 	const configs: Config[][] = data.map((val) => {
 		const shape: [number, number, number] = [1, 1, 1];
 		return data.map((ival) => {
-			const color = val.output < 0.5 ? 'white' : 'black';
+			const color = val.output < 0.5 ? 'white' : 'blue';
 			return <Config>{
-				coords: { x: val.input + 0.5, y: 0.5 * val.output, z: ival.input + 0.5 },
+				coords: { x: ival.input + 0.5, y: 0.5 * val.output, z: val.input + 0.5 },
 				shape,
 				color
 			};
@@ -58,7 +59,7 @@
 	makeDefault
 	position={[10, 10, 10]}
 	oncreate={(ref) => {
-		ref.lookAt(0, 1, 0);
+		ref.lookAt(0, 2, 0);
 	}}
 />
 
@@ -87,7 +88,16 @@
 	backgroundColor="#ffffff"
 	backgroundOpacity={100}
 	gridSize={[10, 10]}
-	position={[0, 0, 0]}
+	position={[0, 0.01, 0]}
+/>
+<Grid
+	sectionColor={color}
+	sectionThickness={1}
+	cellColor="#33334d"
+	backgroundColor="#ffffff"
+	backgroundOpacity={100}
+	gridSize={[10, 10]}
+	position={[0, -0.01, 0]}
 />
 <Grid
 	sectionColor={color}
@@ -107,5 +117,127 @@
 	plane="xy"
 	position={[0, 0, 0]}
 />
+<Grid
+	sectionColor={color}
+	sectionThickness={1}
+	cellColor="#cccccc"
+	gridSize={[10, 4]}
+	axis="x"
+	plane="xy"
+	position={[0, 0, -5]}
+	cellSize={0.5}
+	sectionSize={1}
+/>
+<Grid
+	sectionColor={color}
+	sectionThickness={1}
+	cellColor="#cccccc"
+	gridSize={[10, 4]}
+	axis="x"
+	plane="zy"
+	position={[-5, 0, 0]}
+	cellSize={0.5}
+	sectionSize={1}
+/>
 
 <T.DirectionalLight position={[10, 10, 10]} castShadow />
+
+<HTML position.z={5.1} position.x={5} occlude>
+	<h1>-5</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={4} occlude>
+	<h1>-4</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={3} occlude>
+	<h1>-3</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={2} occlude>
+	<h1>-2</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={1} occlude>
+	<h1>-1</h1>
+</HTML>
+
+<HTML position.z={5.1} occlude>
+	<h1>0</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={-1} occlude>
+	<h1>1</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={-2} occlude>
+	<h1>2</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={-3} occlude>
+	<h1>3</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={-4} occlude>
+	<h1>4</h1>
+</HTML>
+
+<HTML position.z={5.1} position.x={-5} occlude>
+	<h1>5</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={4} occlude>
+	<h1>-4</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={3} occlude>
+	<h1>-3</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={2} occlude>
+	<h1>-2</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={1} occlude>
+	<h1>-1</h1>
+</HTML>
+
+<HTML position.x={5.1} occlude>
+	<h1>0</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-1} occlude>
+	<h1>1</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-2} occlude>
+	<h1>2</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-3} occlude>
+	<h1>3</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-4} occlude>
+	<h1>4</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-5} occlude>
+	<h1>5</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-5} position.y={1.1} occlude>
+	<h1>1</h1>
+</HTML>
+
+<HTML position.x={5.1} position.z={-5} position.y={2.1} occlude>
+	<h1>2</h1>
+</HTML>
+
+<HTML position.x={-5} position.z={5.1} position.y={1.1} occlude>
+	<h1>1</h1>
+</HTML>
+
+<HTML position.x={-5} position.z={5.1} position.y={2.1} occlude>
+	<h1>2</h1>
+</HTML>
