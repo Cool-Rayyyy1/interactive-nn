8fe1bb0f975fbcbc8a8e5db20117b285e5adca80
Wollaston <matt@wollaston.dev>
2025-05-18 10:49:02 -0400

    Update interactive perceptron network charts

--- src/routes/one-input-perceptrons/one-layer-perceptron/+page.svelte
+++ src/routes/one-input-perceptrons/one-layer-perceptron/+page.svelte
@@ -3,19 +3,30 @@
 	import SingleLayerPerceptron from '$lib/components/perceptrons/single-layer-perceptron.svelte';
 	import { setNetworkContext } from '$lib/context';
 	import { SingleLayerOneInputNetwork } from '$lib/networks/single-layer-one-input';
-	import type { Input2d, Network } from '$lib/types';
+	import type { Network, SeriesData } from '$lib/types';
 	import { networkValue2d } from '$lib/utils';
+
+	const keys = ['activation'];
 
 	let network: Network = $state(SingleLayerOneInputNetwork);
 
-	let data: Input2d[] = $derived(networkValue2d(network));
+	let data: SeriesData[] = $derived.by(() => {
+		return networkValue2d(network).map(
+			(val): SeriesData =>
+				<SeriesData>{
+					input: val.input,
+					activation: val.output,
+					derivative: 0
+				}
+		);
+	});
 
 	setNetworkContext(network);
 </script>
 
 <div>
 	<h1>Single-Layer Perceptron</h1>
-	<ActivationChart {data} />
+	<ActivationChart {data} showDx={false} {keys} />
 	<div style:height="500px" class="border-2">
 		<SingleLayerPerceptron />
 	</div>
