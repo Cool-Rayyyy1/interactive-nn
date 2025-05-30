fe1d676d30ea0f3fe3ad44effb66b7e6d15a0e4c
Wollaston <matt@wollaston.dev>
2025-05-29 22:56:40 -0400

    Add welcome page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to Interactive NN!</h1>
-<h2>Please Navigate to the Different Pages using the Sidebar</h2>
+<div class="mt-4 flex justify-center">
+	<article class="prose lg:prose-xl">
+		<h2>Welcome to Interactive NN!</h2>
+		<p>
+			Interactive NN is an interactive learning platform for learning the concepts, math, and
+			intuition behind <span class="text-blue-400">Neural Networks</span> interactively.
+		</p>
+
+		<p>
+			This tool is free and open source software, supported by <span class="font-bold"
+				>NIH Grant #####</span
+			>.
+		</p>
+
+		<p>
+			The learning path for Interacative NN is organized chronologically, and we recommend that
+			students start with the basics in the <a href="/getting-started">Getting Started</a> section. If
+			some concepts are already familiar, they can be skipped.
+		</p>
+
+		<p>
+			Please Navigate to the different sections and pages of the learning path using the sidebar.
+		</p>
+
+		<p>More about this website and tool can be found in the <a href="/about">About</a> page.</p>
+	</article>
+</div>
