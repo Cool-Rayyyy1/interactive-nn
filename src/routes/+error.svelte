889087a53867abbb61e3dcea77dd33e581d94e12
Wollaston <matt@wollaston.dev>
2025-05-29 23:51:32 -0400

    Update fallback

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,6 @@
-<h1>Oops! The requested page cannot be found. Please use the sidebar to get back on track!</h1>
+<div class="mt-4 flex justify-center">
+	<article class="prose lg:prose-xl">
+		<h2>Page Not Found</h2>
+		<p>Oops! The requested page cannot be found. Please use the sidebar to get back on track!</p>
+	</article>
+</div>
