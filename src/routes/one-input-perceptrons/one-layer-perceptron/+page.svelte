04ff00bd6deda1aaade4dc54ddab5169a634623b
Wollaston <matt@wollaston.dev>
2025-05-29 23:57:32 -0400

    Additional style and boilerplate updates

--- src/routes/one-input-perceptrons/one-layer-perceptron/+page.svelte
+++ src/routes/one-input-perceptrons/one-layer-perceptron/+page.svelte
@@ -24,10 +24,22 @@
 	setNetworkContext(network);
 </script>
 
-<div>
-	<h1>Single-Layer Perceptron</h1>
-	<ActivationChart {data} showDx={false} {keys} />
-	<div style:height="500px" class="border-2">
-		<SingleLayerPerceptron />
+<div class="flex w-full justify-center">
+	<div class="mt-4 flex max-w-prose flex-col justify-center">
+		<article class="prose lg:prose-xl">
+			<h2>One Layer, One Input Perceptron</h2>
+			<p>This example is a Perceptron with one layer and one input.</p>
+			<p>
+				It uses a <a href="/activation-functions#step">Step</a> function by default. Experiment with
+				the values!
+			</p>
+		</article>
+
+		<div class="m-2">
+			<ActivationChart {data} showDx={false} {keys} />
+		</div>
+		<div style:height="500px" class="border-2">
+			<SingleLayerPerceptron />
+		</div>
 	</div>
 </div>
--- src/routes/one-input-perceptrons/what-is-a-perceptron/+page.svelte
+++ src/routes/one-input-perceptrons/what-is-a-perceptron/+page.svelte
@@ -1,1 +1,6 @@
-<h1>What is a Perceptron?</h1>
+<div class="mt-4 flex justify-center">
+	<article class="prose lg:prose-xl">
+		<h2>What is a Perceptron?</h2>
+		<p>Future Content</p>
+	</article>
+</div>
