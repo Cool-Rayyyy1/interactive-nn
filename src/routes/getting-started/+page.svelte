e24799830755bc487b2d87b1dfa14c3ca0d129db
Wollaston <matt@wollaston.dev>
2025-07-29 18:30:49 -0400

    Remove mdsvex

---
+++ src/routes/getting-started/+page.svelte
@@ -0,0 +1,24 @@
+<div class="mt-4 flex justify-center">
+	<article class="prose lg:prose-xl">
+		<h2>Getting Started with Neural Networks</h2>
+		<p>
+			Neural Networks are an exciting way to use networks of connected neurons to predict
+			information.
+		</p>
+		<h3 id="what-is-a-neural-network">What is a Neural Network?</h3>
+
+		<p>Neural Networks are networks of neurons.</p>
+		<h3 id="Neurons">Neurons</h3>
+
+		<p>Neurons have activation functions.</p>
+		<h3 id="Inputs">Inputs</h3>
+
+		<p>Inputs are what the inputs to the network are.</p>
+		<h3 id="Outputs">Outputs?</h3>
+
+		<p>Outputs are the transformed values from the network.</p>
+		<h3 id="Perceptrons">Perceptrons</h3>
+
+		<p>These ideas combine to form perceptrons.</p>
+	</article>
+</div>
